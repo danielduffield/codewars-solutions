325f2570455a83eceefa0ba06a9e1849ce3e17e8
Daniel Duffield <danieljduffield@gmail.com>
2017-09-03 15:17:52 -0700

    Created a ChallengeList component that creates a table and maps challenge info as rows. #1

---
+++ client/utils/challengeList.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+class ChallengeList extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <h1 className="text-centered">Codewars Solutions</h1>
+          <table className="table table-bordered text-centered">
+            <thead>
+              <tr className="thead-row">
+                <th className="text-centered challenge-name">Challenge Name</th>
+                <th className="text-centered challenge-info">Author</th>
+                <th className="text-centered challenge-info">Difficulty</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.challenges.map((challenge, index) => {
+                return (
+                  <tr key={index}>
+                    <ChallengeName>{challenge.name}</ChallengeName>
+                    <ChallengeAuthor>{challenge.author}</ChallengeAuthor>
+                    <ChallengeDifficulty>{challenge.difficulty}</ChallengeDifficulty>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+const ChallengeName = styled.td`
+  width: 65%;
+`
+
+const ChallengeAuthor = styled.td`
+  width: 20%;
+`
+
+const ChallengeDifficulty = styled.td`
+  width: 15%;
+`
+
+function mapStateToProps(state) {
+  return {
+    challenges: state.challenges
+  }
+}
+
+const Connected = connect(mapStateToProps)(ChallengeList)
+export default Connected
