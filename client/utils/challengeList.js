7d5d406f5e05e901bb7f02479e32fc201789bd73
Daniel Duffield <danieljduffield@gmail.com>
2017-09-03 18:48:50 -0700

    Moved main title from component render to index.html and added subtitle. #1

--- client/utils/challengeList.js
+++ client/utils/challengeList.js
@@ -5,31 +5,28 @@ import styled from 'styled-components'
 class ChallengeList extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-md-8 col-md-offset-2">
-          <h1 className="text-centered">Codewars Solutions</h1>
-          <table className="table table-bordered text-centered">
-            <thead>
-              <tr className="thead-row">
-                <th className="text-centered challenge-name">Challenge Name</th>
-                <th className="text-centered challenge-info">Author</th>
-                <th className="text-centered challenge-info">Difficulty*</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.challenges.map((challenge, index) => {
-                return (
-                  <tr key={index}>
-                    <ChallengeName>{challenge.name}</ChallengeName>
-                    <ChallengeAuthor>{challenge.author}</ChallengeAuthor>
-                    <ChallengeDifficulty>{challenge.difficulty}</ChallengeDifficulty>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-          <p className="text-centered">*Difficulty increases at lower Kyu ratings</p>
-        </div>
+      <div className="challenge-list-container">
+        <table className="table table-bordered text-centered">
+          <thead>
+            <tr className="thead-row">
+              <th className="text-centered challenge-name">Challenge Name</th>
+              <th className="text-centered challenge-info">Author</th>
+              <th className="text-centered challenge-info">Difficulty*</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.challenges.map((challenge, index) => {
+              return (
+                <tr key={index}>
+                  <ChallengeName>{challenge.name}</ChallengeName>
+                  <ChallengeAuthor>{challenge.author}</ChallengeAuthor>
+                  <ChallengeDifficulty>{challenge.difficulty}</ChallengeDifficulty>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <p className="text-centered">*Difficulty increases at lower Kyu ratings</p>
       </div>
     )
   }
