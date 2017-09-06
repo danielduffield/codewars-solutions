952693830caa0028099186bb40f384ce0f97d691
Daniel Duffield <danieljduffield@gmail.com>
2017-09-05 18:22:42 -0700

    Exported the server for use in mocha testing. #1

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
+const server = app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
 
 app.use(express.static('server/public'))
 
-app.get('/', (req, res) => res.send('Success!'))
+module.exports = server
