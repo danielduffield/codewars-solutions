207d32f32361d0b2630e2c0df0d6d0844bb4f41d
Daniel Duffield <danieljduffield@gmail.com>
2017-09-08 23:06:05 -0700

    Created an HTTP request using the url sent by client and responded to the client with the result. #2

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+const request = require('request')
 
 const server = app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
 
@@ -11,7 +12,18 @@ app.use(express.static('server/public'))
 
 app.post('/submit-url', (req, res) => {
   console.log(req.body.url)
-  res.sendStatus(201)
+  getCodewarsChallenge(req.body.url).then(response => {
+    res.status(201).send(response)
+  })
 })
+
+function getCodewarsChallenge(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) return reject(err)
+      return resolve(response)
+    })
+  })
+}
 
 module.exports = server
