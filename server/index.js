f5dcb2dac73123fd002447f02e22c165633beb45
Daniel Duffield <danieljduffield@gmail.com>
2017-09-08 18:38:04 -0700

    Added an express route to receive client post requests at '/submit-url' and used body-parser to process the request body. #2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 const server = app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
 
+app.use(jsonParser)
 app.use(express.static('server/public'))
+
+app.post('/submit-url', (req, res) => {
+  console.log(req.body.url)
+  res.sendStatus(201)
+})
 
 module.exports = server
