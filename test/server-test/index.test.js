d2f059885b23f4c46ff9c665ae67e02e99b0a49c
Daniel Duffield <danieljduffield@gmail.com>
2017-09-09 14:30:34 -0700

    Added a test to the index.test that checks the server POST route handling. #2

--- test/server-test/index.test.js
+++ test/server-test/index.test.js
@@ -23,6 +23,31 @@ describe('index', () => {
       request.get('http://localhost:' + port, (err, res, body) => {
         expect(err).to.equal(null)
         expect(res).to.have.property('statusCode', 200)
+        console.log('Completed GET test')
+        done()
+      })
+    })
+  })
+
+  describe('POST /submit-url', () => {
+    it('handles POST requests made to /submit-url', done => {
+      const options = {
+        url: 'http://localhost:3000/submit-url',
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ url: 'https://www.codewars.com/kata/snail' })
+      }
+      const req = new Promise((resolve, reject) => {
+        request(options, (err, res, body) => (err ? reject(err) : resolve(res)))
+      })
+      req.then(response => {
+        expect(response.statusCode).to.equal(201)
+        console.log('Completed POST test')
+        done()
+      })
+      .catch(err => {
+        expect(err).to.equal(null)
+        console.log('An error occurred: ', err)
         done()
       })
     })
