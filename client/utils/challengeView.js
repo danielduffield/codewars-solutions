bfee8c6b68c2b86d2c460ab35aae4128615986db
Daniel Duffield <danieljduffield@gmail.com>
2017-09-11 12:34:57 -0700

    Imported styled-components to add styling to elements in the challengeView render. #6

--- client/utils/challengeView.js
+++ client/utils/challengeView.js
@@ -1,26 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 import { connect } from 'react-redux'
 
 class ChallengeView extends React.Component {
   render() {
     return (
-      <div>
-        <h3>{'Codewars Challenge:' + this.props.selectedChallenge.name}</h3>
-        <h4>Challenge Details</h4>
-        <ul>
-          <li>{'Name: ' + this.props.selectedChallenge.name}</li>
-          <li>{'URL: ' + this.props.selectedChallenge.url}</li>
-          <li>{'Difficulty: ' + this.props.selectedChallenge.difficulty}</li>
-          <li>{'Author: '}
-            <a href={this.props.selectedChallenge.authorUrl}>
-              {this.props.selectedChallenge.author}
-            </a>
-          </li>
-        </ul>
+      <div className={this.props.view === 'challengeView' ? '' : 'hidden'}>
+        <ChallengeTitle className="text-center">{'Codewars Challenge: + this.props.selectedChallenge.name'}</ChallengeTitle>
+        <ChallengeDetails>
+          <h4>Challenge Details</h4>
+          <ul>
+            <ChallengeDetail>{'Name: + this.props.selectedChallenge.name'}</ChallengeDetail>
+            <ChallengeDetail>{'URL:  + this.props.selectedChallenge.url'}</ChallengeDetail>
+            <ChallengeDetail>{'Difficulty:  + this.props.selectedChallenge.difficulty'}</ChallengeDetail>
+            <ChallengeDetail>{'Author: '}
+              <a href={'this.props.selectedChallenge.authorUrl'}>
+                {'this.props.selectedChallenge.author'}
+              </a>
+            </ChallengeDetail>
+          </ul>
+        </ChallengeDetails>
+        <ChallengeDescription>
+          <h4>Challenge Description</h4>
+        </ChallengeDescription>
       </div>
     )
   }
 }
+
+const ChallengeTitle = styled.h3`
+  font-weight: bold;
+`
+
+const ChallengeDetails = styled.div`
+  padding: 10px;
+  background-color: lightgrey;
+`
+
+const ChallengeDescription = styled.div`
+  padding: 10px;
+  background-color: lightgrey;
+`
+
+const ChallengeDetail = styled.li`
+  margin: 20px 0 0 20px;
+`
 
 function mapStateToProps(state) {
   return {
