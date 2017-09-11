e875c54f9bcc311cbd4df0d69cbbe4dc2af3e581
Daniel Duffield <danieljduffield@gmail.com>
2017-09-11 12:44:07 -0700

    Mapped selectedChallenge to ChallengeView props to load challenge data in text content of HTML rendered elements. #6

--- client/utils/challengeView.js
+++ client/utils/challengeView.js
@@ -6,22 +6,23 @@ class ChallengeView extends React.Component {
   render() {
     return (
       <div className={this.props.view === 'challengeView' ? '' : 'hidden'}>
-        <ChallengeTitle className="text-center">{'Codewars Challenge: + this.props.selectedChallenge.name'}</ChallengeTitle>
+        <ChallengeTitle className="text-center">{'Codewars Challenge: ' + this.props.selectedChallenge.name}</ChallengeTitle>
         <ChallengeDetails>
           <h4>Challenge Details</h4>
           <ul>
-            <ChallengeDetail>{'Name: + this.props.selectedChallenge.name'}</ChallengeDetail>
-            <ChallengeDetail>{'URL:  + this.props.selectedChallenge.url'}</ChallengeDetail>
-            <ChallengeDetail>{'Difficulty:  + this.props.selectedChallenge.difficulty'}</ChallengeDetail>
+            <ChallengeDetail>{'Name: ' + this.props.selectedChallenge.name}</ChallengeDetail>
+            <ChallengeDetail>{'URL: ' + this.props.selectedChallenge.url}</ChallengeDetail>
+            <ChallengeDetail>{'Difficulty: ' + this.props.selectedChallenge.difficulty}</ChallengeDetail>
             <ChallengeDetail>{'Author: '}
-              <a href={'this.props.selectedChallenge.authorUrl'}>
-                {'this.props.selectedChallenge.author'}
+              <a href={this.props.selectedChallenge.authorUrl}>
+                {this.props.selectedChallenge.author}
               </a>
             </ChallengeDetail>
           </ul>
         </ChallengeDetails>
         <ChallengeDescription>
           <h4>Challenge Description</h4>
+          <p>{this.props.selectedChallenge.description}</p>
         </ChallengeDescription>
       </div>
     )
@@ -48,7 +49,8 @@ const ChallengeDetail = styled.li`
 
 function mapStateToProps(state) {
   return {
-    view: state.view
+    view: state.view,
+    selectedChallenge: state.selectedChallenge
   }
 }
 
