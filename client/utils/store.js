8f4313a7b8a2b244af0a2d99327e3163d6862c01
Daniel Duffield <danieljduffield@gmail.com>
2017-09-11 14:10:20 -0700

    Updated example urls from http to https. #6

--- client/utils/store.js
+++ client/utils/store.js
@@ -4,29 +4,29 @@ function reducer(state = {
   view: 'challengeView',
   selectedChallenge: {
     id: '1',
-    url: 'http://www.codewars.com',
+    url: 'https://www.codewars.com',
     name: 'testChallenge',
     author: 'testAuthor',
-    authorUrl: 'http://www.codewars.com',
+    authorUrl: 'https://www.codewars.com',
     difficulty: '4 kyu',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   challenges: [
     {
       id: '1',
-      url: 'http://www.codewars.com',
+      url: 'https://www.codewars.com',
       name: 'testChallenge',
       author: 'testAuthor',
-      authorUrl: 'http://www.codewars.com',
+      authorUrl: 'https://www.codewars.com',
       difficulty: '4 kyu',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
       id: '2',
-      url: 'http://www.codewars.com/kata/snail',
+      url: 'https://www.codewars.com/kata/snail',
       name: 'Snail',
       author: 'StevenBarragan',
-      authorUrl: 'http://www.codewars.com/user/stevenbarragan',
+      authorUrl: 'https://www.codewars.com/user/stevenbarragan',
       difficulty: '4 kyu',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
