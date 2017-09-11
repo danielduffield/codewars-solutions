0ab6a8771342375985802072490b0ffce24b2ddf
Daniel Duffield <danieljduffield@gmail.com>
2017-09-11 14:49:02 -0700

    Restored default view from 'challengeView' to 'challengeList' and moved example challenges to array 'examples'. #6

--- client/utils/store.js
+++ client/utils/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux'
 
-function reducer(state = {
-  view: 'challengeView',
-  selectedChallenge: {
+const examples = [
+  {
     id: '1',
     url: 'https://www.codewars.com',
     name: 'testChallenge',
@@ -11,26 +10,21 @@ function reducer(state = {
     difficulty: '4 kyu',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
-  challenges: [
-    {
-      id: '1',
-      url: 'https://www.codewars.com',
-      name: 'testChallenge',
-      author: 'testAuthor',
-      authorUrl: 'https://www.codewars.com',
-      difficulty: '4 kyu',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-      id: '2',
-      url: 'https://www.codewars.com/kata/snail',
-      name: 'Snail',
-      author: 'StevenBarragan',
-      authorUrl: 'https://www.codewars.com/users/stevenbarragan',
-      difficulty: '4 kyu',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
-  ]
+  {
+    id: '2',
+    url: 'https://www.codewars.com/kata/snail',
+    name: 'Snail',
+    author: 'StevenBarragan',
+    authorUrl: 'https://www.codewars.com/users/stevenbarragan',
+    difficulty: '4 kyu',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
+
+function reducer(state = {
+  view: 'challengeList',
+  selectedChallenge: examples[0],
+  challenges: examples
 }, action) {
   switch (action.type) {
     case 'UPDATED_VIEW':
