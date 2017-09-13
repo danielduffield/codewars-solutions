e964884e7ab1233c7909105c6658352e38c9bdd0
Daniel Duffield <danieljduffield@gmail.com>
2017-09-13 16:42:11 -0700

    Imported the SolutionForm into the app to be rendered in the component list. #3

--- client/utils/app.js
+++ client/utils/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ChallengeList from './challengeList.js'
 import ChallengeSubmitForm from './challengeSubmitForm.js'
 import ChallengeView from './challengeView.js'
+import SolutionForm from './solutionForm.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ChallengeList/>
       <ChallengeSubmitForm/>
       <ChallengeView/>
+      <SolutionForm/>
     </div>
   )
 }
