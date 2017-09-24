5dd04ce2c7d44939e206452be7c84bc3552e5a41
Daniel Duffield <danieljduffield@gmail.com>
2017-09-24 15:56:10 -0700

    Added a migration file for inserting the second row into the challenges table. #9

---
+++ migrations/1506293396533_insert_challenge_2.sql
@@ -0,0 +1,10 @@
+-- up
+INSERT INTO challenges (id, url, name, author, author_url, difficulty)
+VALUES ('2', 'https://www.codewars.com/kata/snail', 'Snail', 'StevenBarragan', 'https://www.codewars.com/users/stevenbarragan', '4 kyu')
+
+
+---
+
+-- down
+
+TRUNCATE TABLE challenges
