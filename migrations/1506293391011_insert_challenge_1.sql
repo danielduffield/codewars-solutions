787c4ec031af49e7678e193f11a8373c94294e89
Daniel Duffield <danieljduffield@gmail.com>
2017-09-24 15:55:33 -0700

    Added a migration file for inserting the first row into the challenges table. #9

---
+++ migrations/1506293391011_insert_challenge_1.sql
@@ -0,0 +1,10 @@
+-- up
+INSERT INTO challenges (id, url, name, author, author_url, difficulty)
+VALUES ('1', 'https://www.codewars.com', 'testChallenge', 'testAuthor', 'https://www.codewars.com', '4 kyu')
+
+
+---
+
+-- down
+
+TRUNCATE TABLE challenges
