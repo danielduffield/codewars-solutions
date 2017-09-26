c84f143e0c576d6976b8036cec90285a7cfae4e1
Daniel Duffield <danieljduffield@gmail.com>
2017-09-26 12:14:36 -0700

    Removed unnecessary console log used for debugging. #17

--- server/utils/addChallenge.js
+++ server/utils/addChallenge.js
@@ -2,7 +2,6 @@ const { knexInsert } = require('./knexCommands.js')
 const omit = require('./omit.js')
 
 function addChallenge(challengeData) {
-  console.log('ADDED CHALLENGE TO DB')
   const challenge = omit(challengeData.challenge, ['authorUrl'])
   challenge.author_url = challengeData.challenge.authorUrl
   console.log(challenge)
