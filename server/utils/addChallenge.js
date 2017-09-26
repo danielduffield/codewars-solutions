9efd4d942bfa1372205f9d17fc933bb3c773126d
Daniel Duffield <danieljduffield@gmail.com>
2017-09-26 12:38:13 -0700

    Returned data from the knex insert to add challenge id to list of existing ids. #17

--- server/utils/addChallenge.js
+++ server/utils/addChallenge.js
@@ -4,8 +4,8 @@ const omit = require('./omit.js')
 function addChallenge(challengeData) {
   const challenge = omit(challengeData.challenge, ['authorUrl'])
   challenge.author_url = challengeData.challenge.authorUrl
-  console.log(challenge)
-  return knexInsert('challenges', challenge)
+  console.log('ADDING ', challenge)
+  return knexInsert('challenges', challenge).returning('*')
 }
 
 module.exports = addChallenge
