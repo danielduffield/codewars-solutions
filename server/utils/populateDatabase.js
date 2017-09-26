68e5a7876db1bdf961469a6ffdb2e807faeb87ae
Daniel Duffield <danieljduffield@gmail.com>
2017-09-26 11:55:42 -0700

    Refactored populateDatabase to add the parameter existingIds to check against existingIds before inserting challenge data. #17

--- server/utils/populateDatabase.js
+++ server/utils/populateDatabase.js
@@ -5,7 +5,7 @@ const getCodewarsChallenge = require('./getCodewarsChallenge.js')
 const parseApiData = require('./parseApiData.js')
 // const { knexInsert } = require('./knexCommands.js')
 
-function populateDatabase() {
+function fetchAllChallenges() {
   const filePath = path.join(__dirname, '../solutions')
   console.log(filePath)
   const loadingFiles = fs.readdir(filePath)
@@ -20,6 +20,18 @@ function buildChallenge(fileName) {
     const challengeData = parseApiData(JSON.parse(response.body))
     return challengeData
   }).catch(err => console.log('Couldn\'t fetch: ', err))
+}
+
+function populateDatabase(existingIds) {
+  console.log('EXISTING IDS: ', existingIds)
+  fetchAllChallenges().then(challengeList => {
+    console.log('SOLUTION CHALLENGES: ', challengeList)
+    challengeList.forEach(challengeData => {
+      if (existingIds.includes(challengeData.challenge.id)) {
+        console.log('Found duplicate: ', challengeData.challenge.id)
+      }
+    })
+  })
 }
 
 module.exports = populateDatabase
