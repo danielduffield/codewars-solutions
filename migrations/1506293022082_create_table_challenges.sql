028f943f15f587f5a6ae16dc854bcb2ffafd292a
Daniel Duffield <danieljduffield@gmail.com>
2017-10-02 11:09:34 -0700

    Increased the maximum length of the name value within the challenges table to account for challenges with longer names. #26

--- migrations/1506293022082_create_table_challenges.sql
+++ migrations/1506293022082_create_table_challenges.sql
@@ -3,7 +3,7 @@
 create table challenges (
   id VARCHAR(30),
   url VARCHAR(50),
-  name VARCHAR(30),
+  name VARCHAR(40),
   author VARCHAR(20),
   author_url VARCHAR(50),
   difficulty VARCHAR(10)
