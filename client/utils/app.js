ce19c2403b686f93ca142ad5330d6637e8a0f770
Daniel Duffield <danieljduffield@gmail.com>
2017-10-30 15:45:56 -0700

    Added the hash as data sent when dispatching the fetched data recieved event to parse and route the hash on page load. #19

--- client/utils/app.js
+++ client/utils/app.js
@@ -1,18 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import ChallengeList from './challengeList.js'
 import ChallengeSubmitForm from './challengeSubmitForm.js'
 import ChallengeView from './challengeView.js'
 import SolutionSubmitForm from './solutionSubmitForm.js'
+import socket from './socket-connection'
 
-function App() {
-  return (
-    <div>
-      <ChallengeList/>
-      <ChallengeSubmitForm/>
-      <ChallengeView/>
-      <SolutionSubmitForm/>
-    </div>
-  )
+class App extends React.Component {
+  componentDidMount() {
+    socket.on('fetchedData', fetchedChallenges => {
+      const challengeList = fetchedChallenges.map(challengeData => challengeData.challenge)
+      const fetched = fetchedChallenges.filter(challengeData => challengeData.description)
+      this.props.dispatch({
+        type: 'RECEIVED_FETCHED_DATA',
+        payload: {
+          fetched,
+          challengeList,
+          hash: window.location.hash.replace('#', '')
+        }
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <ChallengeList/>
+        <ChallengeSubmitForm/>
+        <ChallengeView/>
+        <SolutionSubmitForm/>
+      </div>
+    )
+  }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    view: state.view
+  }
+}
+
+const Connected = connect(mapStateToProps)(App)
+export default Connected
