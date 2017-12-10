39142bbd6eadbfba6b9223c17c0585eb1a67d3a0
Daniel Duffield <danieljduffield@gmail.com>
2017-12-10 12:32:26 -0800

    Increased the number of characters in the url and author url to prevent insertion failure for longer names. #32

--- migrations/1506293022082_create_table_challenges.sql
+++ migrations/1506293022082_create_table_challenges.sql
@@ -2,10 +2,10 @@
 
 create table challenges (
   id VARCHAR(30),
-  url VARCHAR(50),
+  url VARCHAR(60),
   name VARCHAR(40),
   author VARCHAR(20),
-  author_url VARCHAR(50),
+  author_url VARCHAR(60),
   difficulty VARCHAR(10)
 );
 
